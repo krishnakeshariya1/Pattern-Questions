62572419132add600b8f73302c5ae3ece6dc7d54
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-05 15:01:43 +0530

    Day-1 completed

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,1 +1,47 @@
-// Question-1
+// Question 1 - solid square
+for(let i =1; i<=5; i++){
+    let row = "";
+    for(let j =1; j<=5; j++){
+        row += "*";
+    }
+    console.log(row);
+}
+
+// Question 2 - Right Angled Trinagle
+
+for(let i =1; i<=5; i++){
+    let row = "";
+    for(let j =1; j<=i; j++){
+        row += "*";
+    }
+    console.log(row);
+}
+
+// Question 3 - reverse Right Angles Triangle
+for(let i =5; i >=1; i--){
+    let row ="";
+    for(let j =1; j<=i; j++){
+         row += "*";
+    }
+     console.log(row)
+}
+
+// Question 4 - Increasing Number Triangle
+
+for(let i =1; i<=5; i++){
+    let row = "";
+    for(let j =1; j<=i; j++){
+        row += j +" ";
+    }
+    console.log(row);
+}
+
+//Question 5 - Decreasing Number Traingle
+
+for(let i =5; i >=1; i--){
+    let row ="";
+    for(let j =1; j<=i; j++){
+         row += j+" ";
+    }
+     console.log(row)
+}
