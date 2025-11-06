01476b2b04b79c0b285c492db5405511a1829c8f
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-06 15:52:45 +0530

    added more question

--- Section-1/script.js
+++ Section-1/script.js
@@ -47,3 +47,60 @@ for(let i =5; i >=1; i--){
 }
 
 //Question 6 - Right Angle triangle 
+let n =5
+for(let i =1; i<= n; i++){
+    let row ="";
+    for(let j =1; j<= n-i; j++ ){
+        row +=" ";
+    }
+    for(let k =1; k<= i; k++){
+        row+= "*";
+    }
+    console.log(row );
+}
+
+//Question 7 - Inverted -Right- Angled triangle
+for(let i=1; i<=n; i++){
+    let row="";
+    for(let j=1; j<i; j++){
+        row+=" ";
+    }
+    for(let k=1; k<= (n-i)+1; k++){
+        row+="*"
+    }
+    console.log(row)
+}
+
+// Question 8 - Centered pyramid
+for(let i =1; i<=n; i++){
+    let row= "";
+    for(let j =1; j<=n-i; j++){
+        row+=" ";
+    }
+    for(let k =1; k<= 2*i-1; k++){
+        row+="*";
+    }
+    console.log(row)
+}
+// Question 9 - inverted pyramid
+ for (let i = 1; i <=n; i++){
+    let row ="";
+    for(let j =1; j<i; j++){
+        row+=" ";
+    }
+    for(let k =1 ; k<= 2*(n-i)+1; k++){
+        row+="*"
+    }
+    console.log(row)
+}
+// Question 10 Hollow pyramid
+for(let i =1; i <=n;i++){
+    let row ="";
+    for(let j=1; j<=n-i; j++ ){
+        row+=" ";
+    }
+   for (let k = 1; k <= 2 * i - 1; k++) {
+    (k === 1 || k === 2 * i - 1 || i === n) ? row += '*' : row += ' ';
+   }
+   console.log(row)
+}
